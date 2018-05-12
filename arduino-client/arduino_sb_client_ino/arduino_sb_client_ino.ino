18f9c72803d6a9e1a576ecb2b0085a3952d157e4
Jan Kozak <galvani78@gmail.com>
2018-05-12 21:45:44 +0200

    add arduino client, remove libraries add supervisord to test

--- arduino-client/arduino_sb_client_ino/arduino_sb_client_ino.ino
+++ arduino-client/arduino_sb_client_ino/arduino_sb_client_ino.ino
@@ -1,12 +1,9 @@
-/** RF24Mesh_Example_Node2Node.ino by TMRh20
- *
- * This example sketch shows how to communicate between two (non-master) nodes using
- * RF24Mesh & RF24Network
+/**
+ * Depends on RF24, RF24Network, RF24Mesh
+ * Depends on the following Arduino libraries:
+ *  * Adafruit Unified Sensor Library: https://github.com/adafruit/Adafruit_Sensor
+ *  * DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
  **/
-
-// Depends on the following Arduino libraries:
-// - Adafruit Unified Sensor Library: https://github.com/adafruit/Adafruit_Sensor
-// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
 
 #include "RF24.h"
 #include "RF24Network.h"
@@ -33,17 +30,20 @@ RF24Mesh mesh(radio, network);
  * otherNodeID - A unique identifier for the 'other' radio
  *
  **/
-#define nodeID 2
-#define otherNodeID 0
+#define nodeID 4
+#define masterNodeId 0
 
-#define DHTPIN            2         // Pin which is connected to the DHT sensor
+#define DHTPIN           4         // Pin which is connected to the DHT sensor
+// #define DHTTYPE           DHT22     // DHT 11
 #define DHTTYPE           DHT11     // DHT 11
 
 //#################################
 DHT_Unified dht(DHTPIN, DHTTYPE);
 
-uint32_t delayTime = 20000;
+uint32_t delayTime = 45000;
 uint32_t delaySensor;
+uint32_t infoFrequency = 100;
+uint32_t infoRunLeft = infoFrequency + 1;
 
 // Structure of our payload
 struct payload_t
@@ -52,14 +52,14 @@ struct payload_t
  uint32_t humidity;
 };
 
+String nodeInfo;
+
 void setup() {
   Serial.begin(115200);
-  //printf_begin();
+  printf_begin();
+
   // Set the nodeID manually
   mesh.setNodeID(nodeID);
-  // Connect to the mesh
-  Serial.println(F("Connecting to the mesh..."));
-  mesh.begin();
   
   // Initialize device.
   dht.begin();
@@ -70,28 +70,65 @@ void setup() {
   Serial.println("------------------------------------");
   Serial.println("Temperature");
   Serial.print  ("Sensor:       "); Serial.println(sensor.name);
-  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
-  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
   Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" *C");
   Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" *C");
   Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" *C");  
   Serial.println("------------------------------------");
+  nodeInfo += String(sensor.name);
+  nodeInfo += String(", ") + String(sensor.min_value) + String("-");
+  nodeInfo += String(sensor.max_value) + String("*C ~") + String(sensor.resolution) + String("*C, ");
+  
   // Print humidity sensor details.
   dht.humidity().getSensor(&sensor);
   Serial.println("------------------------------------");
   Serial.println("Humidity");
   Serial.print  ("Sensor:       "); Serial.println(sensor.name);
-  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
-  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
   Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println("%");
   Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println("%");
   Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println("%");  
   Serial.println("------------------------------------");
+  nodeInfo += String(", ") + String(sensor.min_value) + String("-");
   // Set delay between sensor readings based on sensor details.
   delaySensor = sensor.min_delay / 1000;
+  
+  nodeInfo += String(sensor.max_value) + String("% ~") + String(sensor.resolution) + String("%, delay:") + String(delayTime/1000);
+
+  // Connect to the mesh
+  Serial.println(F("Connecting to the mesh..."));
+  mesh.begin();
+  Serial.println(String("Update frequency is ...") + String(delayTime/1000) + String(" seconds..."));
+  sendInfo();
 }
 
+void sendInfo() {
+  char charBuf[nodeInfo.length()+1];
+  nodeInfo.toCharArray(charBuf, nodeInfo.length()+1);
 
+  bool sent = false;
+
+  while (!sent) {
+    Serial.print(F("Sending node info... ")); 
+    Serial.print(sizeof(charBuf));
+    Serial.print(F(" bytes... ")); 
+    // Send to the master node every ~delayTime miliseconds
+    if (!mesh.write(&charBuf, 'I', sizeof(charBuf), masterNodeId)) {
+        Serial.println(F("failed."));
+        delay(5000);
+        if ( ! mesh.checkConnection() ) {
+          Serial.println(F("renewing address..."));
+          mesh.renewAddress();
+        } else {
+          Serial.println(F("fail, Test OK"));
+          sent = true;
+        }
+      } else {
+        Serial.println(F("Send OK."));
+        sent = true;
+        delay(1);
+      }
+    }
+    delay(100);
+}
 
 void loop() {
   mesh.update();
@@ -105,12 +142,13 @@ void loop() {
   
   if (isnan(eventT.temperature)) {
     Serial.println("Error reading temperature!");
+    Serial.println(eventT.temperature);
   }
   else {
     payload.temperature = eventT.temperature;
     Serial.print("Temperature: ");
     Serial.print(eventT.temperature);
-    Serial.println(" *C");
+    Serial.print(" *C, ");
   }
   // Get humidity event and print its value.
   dht.humidity().getEvent(&eventH);
@@ -121,13 +159,12 @@ void loop() {
     payload.humidity = eventH.relative_humidity;
     Serial.print("Humidity: ");
     Serial.print(eventH.relative_humidity);
-    Serial.println("%");
+    Serial.print("% ...");
   }
 
   
-  // Send to the master node every second
-  // Send an 'M' type to other Node containing the current millis()
-  if (!mesh.write(&payload, 'H', sizeof(payload), otherNodeID)) {
+  // Send to the master node every ~delayTime miliseconds
+  if (!mesh.write(&payload, 'H', sizeof(payload), masterNodeId)) {
       Serial.println(F("Send fail"));
       if ( ! mesh.checkConnection() ) {
         Serial.println(F("Renewing Address"));
@@ -136,29 +173,15 @@ void loop() {
         Serial.println(F("Send fail, Test OK"));
       }
     } else {
-      Serial.print(F("Send OK.")); 
+      Serial.println(F("Send OK.")); 
     }
 
-  delay(delayTime);
+  infoRunLeft--;
 
-//  
-//    stringTimer = millis();
-//    //Copy the current number of characters to the temporary array
-//    memcpy(tmpStr, dataStr, strCtr);
-//    //Set the last character to NULL
-//    tmpStr[strCtr] = '\0';
-//
-//    // Send the temp string as an 'S' type message
-//    // Send it to otherNodeID (An RF24Mesh address lookup will be performed)
-//    //bool ok = mesh.write(tmpStr,'S',strCtr+1,otherNodeID);
-//    if (mesh.write(tmpStr, 'S', strCtr + 1, otherNodeID)) {
-//      strCtr++;
-//      delayTime = 333;
-//      //Set the sending length back to 1 once max size is reached
-//      if (strCtr == sizeof(dataStr)) {
-//        strCtr = 1;
-//      }
-//      //if(strCtr == 12){ strCtr=1; }
-//    }
-//  }
+  if (infoRunLeft==0) {
+    sendInfo();
+    infoRunLeft = infoFrequency;
+  }
+
+  delay(delayTime);
 }
